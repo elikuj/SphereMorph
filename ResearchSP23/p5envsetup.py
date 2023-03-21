3541f6f41c348a9b82295d06d81a99a5fd719a0a
eli <98191154+elikuj@users.noreply.github.com>
2023-03-21 15:52:50 -0500

    Update p5envsetup.py

--- ResearchSP23/p5envsetup.py
+++ ResearchSP23/p5envsetup.py
@@ -1,6 +1,15 @@
 from p5 import *
 import numpy as np
 
+
+from scipy.spatial.transform import Rotation as R
+
+"""import numpy as np
+from scipy.ndimage.interpolation import rotate
+
+x = np.random.randint(800, 1000, size=[100, 100, 3])
+rotated = rotate(x, angle=45)"""
+
 octahedron = [
     [200, 0, 0],
     [0, 0, 200],
@@ -11,11 +20,10 @@ octahedron = [
     [0, -200, 0],
     [0, 0, -200],
     [200, 0, 0],
-    
     [0, 200, 0],
     [-200, 0, 0],
     [0, -200, 0],
-    [200, 0, 0]
+    [200, 0, 0]                           
 ]
 
 def plotShape(shape):
@@ -32,6 +40,26 @@ def projectToSphere(shape, radius):
         for j in range(0, 21):
             toplot = ((first + dif*j/20))
             toplot = toplot * radius/np.linalg.norm(toplot)    
+            vertex(toplot[0], toplot[1], toplot[2])
+    end_shape()
+
+# projects shape to sphere w/ given radius
+# keeps distribution of points on sphere more equal
+def projectByAngle(shape, radius):
+    begin_shape()
+    for i, pt in enumerate(shape):
+        first_proj = np.array(pt)/(np.linalg.norm(pt))*radius
+        next_proj = np.array(shape[(i+1)%len(shape)])/(np.linalg.norm(shape[(i+1)%len(shape)]))*radius
+        normal = np.cross(first_proj, next_proj)
+        normal = normal/np.linalg.norm(normal)
+        theta = 2*math.acos(np.dot(first_proj, next_proj)/radius**2)
+        divs = (int)(np.linalg.norm(np.array(pt) - np.array(shape[(i+1)%len(shape)]))/10)
+        toplot = first_proj
+        print("START")
+        for i in range(0, divs):
+            rotvec = R.from_rotvec(normal * theta/divs)
+            toplot = rotvec.apply(toplot)
+            print(f"[ {toplot[0]}, {toplot[1]}, {toplot[2]}],")
             vertex(toplot[0], toplot[1], toplot[2])
     end_shape()
 
@@ -60,22 +88,31 @@ def stretch(shape, factor: float, dir: int):
     return cop
 
 def setup():
-    noFill()
+    #noFill()
     size(1000, 1000)
+    
+    #lights()
+    #directional_light(128, 128, 200, 0, 0, -1)
+    
     
 #stretchedoct = stretch(octahedron.copy(), 2, 0)
 
 
 def draw():
-    background(150)
-    camera(4*mouse_x, 4*mouse_y, (height/2.0)/(math.tan(PI/6)), 0, 0, 0, 0, 1, 0) #BEST
     
+    background(120)
+    lights()
+    ambient_light(100, 0, 0)
+    
+    camera(4*(mouse_x-width/2), 4*mouse_y, (height/2.0)/(math.tan(PI/6)), 0, 0, 0, 0, 1, 0) #BEST
+    #pointLight(51, 102, 126, 80, 20, 40)
     #adding vertex [200, 200, 200]
     #newoct = addVertex([200, 200, 200], octahedron, [0, 1, 4])
 
     #add shape
     plotShape(octahedron)
     projectToSphere(octahedron, 100)
+    
 
     
     """plotShape(stretchedoct)
@@ -88,10 +125,17 @@ def draw():
         oct = stretch(octahedron, 1.2, 0)
         plotShape(oct)
         projectToSphere(oct, 100)
+        #projectByAngle(oct, 100)
+
+    if key_is_pressed and key == '2':
+        oct = stretch(octahedron, 1.2, 0)
+        plotShape(oct)
+        #projectToSphere(oct, 100)
+        projectByAngle(oct, 100)
         
     
-    
-    
+# subdision on actual sphere based on angle of curve 
+# 
     
 
     

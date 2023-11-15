0d87dda43c5eb085f08ad7b65586f64b927ae40e
elikuj <ekujawa03@gmail.com>
2023-11-15 14:16:08 -0600

    updates

--- breaking-lemmas/data-structures.py
+++ breaking-lemmas/data-structures.py
@@ -4,7 +4,7 @@ import math
 import random
 
 
-
+# ignore this
 class sphericalEmbedding:
     longs = []
     lats = []
@@ -13,12 +13,20 @@ class sphericalEmbedding:
     adjacencies = []
     
     
-    def __init__(self, lats, longs, adjacencies, radius=1, O = np.array([0,0,0])):
+    def __init__(self, lats, longs, adjacencies, radius=1, O=np.array([0,0,0])):
         self.lats = lats
         self.longs = longs
+        self.origin = O
+        self.radius = radius
+        self.adjacencies = adjacencies
         
     def triVol(a, b, c):
         return la.det(np.array[a,b,c])
+    
+    
+class planarEmbedding:
+    vtxs = []
+    
         
 
 # a = np.array([math.sqrt(2),math.sqrt(2),0])
@@ -30,31 +38,35 @@ class sphericalEmbedding:
 # print(la.det(np.array([a,b,-1*c])))
 
 
+# helper functions
 def random_northern_vertex():
     theta = random.random()*math.pi/2
-    phi = random.random()*math.pi*2
+    phi = random.random()*math.pi
     return (theta, phi)
 
 def random_southern_vertex():
-    theta = random.random()*math.pi/2
-    phi = random.random()*math.pi*2
+    theta = -1*random.random()*math.pi/2
+    phi = random.random()*math.pi
     return (theta, phi)
     
 def coords(vtx):
     theta = vtx[0]
     phi = vtx[1]
-    return 100*np.array([np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)])
+    return np.array([np.cos(theta)*np.cos(phi), np.cos(theta)*np.sin(phi), np.sin(theta)])
 
+# fix latitude
 def fix_north(vtx):
     phi = vtx[1]
     theta = np.pi/4
-    return np.array([np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)])
+    return np.array([np.cos(theta)*np.cos(phi), np.cos(theta)*np.sin(phi), np.sin(theta)])
 
 def fix_south(vtx):
     phi = vtx[1]
-    theta = -np.pi/4
-    return np.array([np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)])
+    theta = -1*np.pi/4
+    return np.array([np.cos(theta)*np.cos(phi), np.cos(theta)*np.sin(phi), np.sin(theta)])
 
+
+# compute determininant of original triangle abc and its fixed-latitude analog; check if signs are different
 def test_dets(a, b, c):
     # det of original triangle:
     og_tri = np.array([a, b, c])
@@ -64,8 +76,8 @@ def test_dets(a, b, c):
     fixed_tri = np.array([fixed_a, fixed_b, fixed_c])
     og_det = la.det(og_tri)
     fixed_det = la.det(fixed_tri)
-    print(og_det)
-    print(fixed_det)
+    #print(og_det)
+    #print(fixed_det)
     if og_det*fixed_det >= 0:
         print("No self-intersection!")
         return True
@@ -73,6 +85,9 @@ def test_dets(a, b, c):
         print("❌ SELF INTERSECTION!!! ❌")
         return False
         
+        
+# run "trials" num of simulations.
+# currently returns "self intersection" about 50% of times, but there's probably something wrong with it
 def simulate(trials):
     fails = 0
     for i in range(trials):
@@ -83,5 +98,5 @@ def simulate(trials):
             fails += 1
     return fails
 
-print(simulate(1000))
+print(simulate(100))
     
